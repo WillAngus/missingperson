c082a7f5150c908f4dc4c415b17ad298109af311
WillAngus <willzter@hotmail.co.uk>
2019-11-22 18:29:34 +0000

    Upload Site

---
+++ js/img.js
@@ -0,0 +1,6 @@
+var home_img_1 = new Image();
+home_img_1.src = 'img/home/carousel/img-1.jpg';
+var home_img_2 = new Image();
+home_img_2.src = 'img/home/carousel/img-2.jpg';
+var home_img_3 = new Image();
+home_img_3.src = 'img/home/carousel/img-3.jpg';
